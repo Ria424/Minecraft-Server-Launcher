1bac800edf531fd3039293762a69af8c555a440e
Ria <1happyprogramming@gmail.com>
2024-06-15 17:21:16 +0900

    Format build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
-pyinstaller -F -n "Minecraft Server Launcher" -i "NONE" --specpath .\export --distpath .\export\dist --workpath .\export\build --clean main.py
+pyinstaller --noconfirm --clean --onefile --console ^
+    --specpath .\export ^
+    --distpath .\export\dist ^
+    --workpath .\export\build ^
+    --name "Minecraft Server Launcher" ^
+    --icon "NONE" ^
+    main.py
 pause
