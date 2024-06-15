53ca9fa85f4e7856ffbb4200f9bc77f9eb919354
Ria <1happyprogramming@gmail.com>
2024-06-15 16:50:12 +0900

    Remove gui related

--- src/gui/util.py
+++
@@ -1,4 +0,0 @@
-from tkinter import Misc
-
-def center_window(master: Misc, width: int, height: int):
-    return f"{width}x{height}+{(master.winfo_screenwidth() - width) // 2}+{(master.winfo_screenheight() - height) // 2}"
